e560a9892bfe0cb3d90391363027e8c309960839
zhoubeautiful <1403953456@qq.com>
2020-06-30 22:06:27 +0800

    配置完成

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
-  <div class="app">我是根组件</div>
+  <div class="app">我是根组件
+    <div class="box"></div>
+  </div>
 </template>
 
 <script>
 export default {
-
 }
 </script>
 
 <style>
-
+.box{
+  width: 300px;
+  height: 300px;
+  font-size: 75px;
+  background-color: black;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 需要显示在页面上的都要导入
+import { Button, Field } from 'vant'
+// import 'vant/lib/index.css'
+import 'amfe-flexible'
+
+// 全局导入vant
+// Vue.use(Vant)
+Vue.use(Button)
+Vue.use(Field)
 
 Vue.config.productionTip = false
 
